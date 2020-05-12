8732c98af98f4b5cbc73d3269328cd2148bdae66
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-12 20:35:29 -0300

    chore: Aplicar novo lint

--- state.ts
+++ state.ts
@@ -1,23 +1,23 @@
 import { Todo } from "./Models/Todo.ts";
 
 export interface AppState<T> {
-    get(): Set<T>;
-    insert(newObject: T): void;
-    remove(existingObject: T): void;
+  get(): Set<T>;
+  insert(newObject: T): void;
+  remove(existingObject: T): void;
 }
 
 export class TodoState implements AppState<Todo> {
-    private todos = new Set<Todo>();
-    
-    get(){
-        return this.todos;
-    }
+  private todos = new Set<Todo>();
 
-    insert(newTodo: Todo): void{
-        this.todos.add(newTodo)
-    }
+  get() {
+    return this.todos;
+  }
 
-    remove(todo: Todo): void{
-        this.todos.delete(todo);
-    }
+  insert(newTodo: Todo): void {
+    this.todos.add(newTodo);
+  }
+
+  remove(todo: Todo): void {
+    this.todos.delete(todo);
+  }
 }
