43bb9f2c3682ebeae509185e5e4bba5f4506dbb8
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-12 20:34:20 -0300

    feat: Define state para a API

---
+++ state.ts
@@ -0,0 +1,23 @@
+import { Todo } from "./Models/Todo.ts";
+
+export interface AppState<T> {
+    get(): Set<T>;
+    insert(newObject: T): void;
+    remove(existingObject: T): void;
+}
+
+export class TodoState implements AppState<Todo> {
+    private todos = new Set<Todo>();
+    
+    get(){
+        return this.todos;
+    }
+
+    insert(newTodo: Todo): void{
+        this.todos.add(newTodo)
+    }
+
+    remove(todo: Todo): void{
+        this.todos.delete(todo);
+    }
+}
