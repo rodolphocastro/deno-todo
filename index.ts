ab47f429bfabb840ea72c14e055bcc82bfe3b810
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-12 20:23:32 -0300

    refactor: Extrair Todo para arquivo próprio

--- index.ts
+++ index.ts
@@ -1,20 +1,15 @@
+import { Todo } from "./Models/Todo.ts";
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-interface Todo {
-    id: string;
-    title: string;
-    createdOn: Date;
-}
-
 const todoDatabase = new Map<string, Todo>();
-todoDatabase.set('myTodo', {
-    id: "myTodo",
+todoDatabase.set('myTodo', {    
     createdOn: new Date(),
-    title: "My Todo"
+    title: "My Todo",
+    description: "My first todo"
 })
 
 const router = new Router();
-router
+router    
     .get("/", (ctx) => {
         ctx.response.body = 'my api';
     })
@@ -24,5 +19,7 @@ router
 
 const app = new Application();
 app.use(router.routes());
+
+console.log("Starting API");
 
 await app.listen("127.0.0.1:8000");
